d19bb1e07e9bd5a17bbcd3ee516ad1afd345e93b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-05-06 09:51:56 +0500

    render post was finished

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
     const [postData, setPostData] = useState({
@@ -13,9 +15,14 @@ const Form = () => {
         selectedFile: "",
     });
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {};
-    const clear = () => {}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
+    };
+    const clear = () => {};
 
     return (
         <Paper className={classes.paper}>
